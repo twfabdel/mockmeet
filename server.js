7eb6e12c5062a6c629a8c5333f84a35a7cd55348
twfabdel <twfabdel@gmail.com>
2016-08-26 14:22:22 -0700

    .

--- server.js
+++ server.js
@@ -3,18 +3,15 @@ var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 // set the home page route
 app.get('/', function(req, res) {
 
-    // ejs render automatically looks in the views folder
+    // make sure index is in the right directory. In this case /app/index.html
     res.render('index');
 });
 
